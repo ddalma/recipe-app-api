3ea144e1d3980d74adab768bf6e3941a8fd33295
Daryl Dalman <dd1574@nyu.edu>
2019-10-12 19:46:43 -0400

    Configured postgres db

--- app/core/admin.py
+++ app/core/admin.py
@@ -21,7 +21,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email','password1', password2)
+            'fields': ('email','password1', 'password2')
             }),
     )
 
